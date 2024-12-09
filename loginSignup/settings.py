e2019867762c7a5ebbfd21a8155931e570457d2f
Aljhon <maligroaljhon@gmail.com>
2024-12-09 23:03:13 +0800

    asdasd

--- loginSignup/settings.py
+++ loginSignup/settings.py
@@ -130,7 +130,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = '/media\\'
+MEDIA_URL = '/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
