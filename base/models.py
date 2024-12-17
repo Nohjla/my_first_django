ce492dd338ca1b401b10ad97af3a40939f3c1699
Nohjla <maligroaljhon@gmail.com>
2024-12-17 23:42:12 +0800

    Update models.py

--- base/models.py
+++ base/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class UploadedFile(models.Model):
+    file = models.FileField(upload_to='uploads/')  # Files will be stored under /var/www/media/uploads
